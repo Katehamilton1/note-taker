b7932130f8a1920030934dfcd598c1dafa1b029d
Kate Hamilton <kate.hamilton828@gmail.com>
2021-12-14 09:49:05 +0300

    updating files

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-// Dependencies
-// =============================================================
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
