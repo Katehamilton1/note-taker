3375001e74b62c24d00ed9a0dc7eb43d40654d43
Kate Hamilton <kate.hamilton828@gmail.com>
2021-12-16 21:07:37 +0300

    update html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,15 @@
 // Path package to get the file path for the html
 var path = require("path");
+const router = require("express").Router();
+
+// GET request
+router.get("/notes", (request, response) => {
+    response.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// Return to Homepage 
+router.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
