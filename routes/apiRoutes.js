c91837e8977104142a27653ae5653d164fb4cdd3
Kate Hamilton <kate.hamilton828@gmail.com>
2021-12-16 20:24:29 +0300

    router.delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-// const fs = require("fs");
+const fs = require("fs");
 const path = require("path");
 
 const db = require("../db/db.json")
@@ -6,25 +6,50 @@ const router = require("express").Router();
 
 
 
-    // API GET Request-- find all notes
-    router.get("/api/notes",  (request, response) => {
-        console.log(db)
-        response.json(db);
-    })
+// API GET Request-- find all notes
+router.get("/api/notes", (request, response) => {
+    console.log(db)
+    response.json(db);
+})
 
 
-     // create a new note 
-    router.post("/api/notes",(request, response) => {
-        db.push(request.body);
-        response.json("saved");
-      });
-   
+// create a new note 
+router.post("/api/notes", (request, response) => {
+    db.push(request.body);
+    response.json("saved");
 
-    // API DELETE note Request 
-    router.delete("/api/user/id:", (request, response)=> {
-        notes.splice(request.params.id,1);
-
-        console.log("Deleted note with id " + request.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), function (err) {
+        if (err) throw (err);
     });
 
-    module.exports = router;
+    res.json(data);
+
+});
+
+// API DELETE note Request 
+router.delete("/api/notes/:id", (request, response) => {
+
+    let id = request.params.id;
+
+    // Read datainfo from db.json 
+    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+    for (let i = 0; i < db.length; i++) {
+        if (id === db[i].id) {
+          db.splice(i, 1);
+          
+    //write to db.json
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes), function (err) {
+        if (err) throw err;
+    })
+
+    response.json(notes);
+});
+
+
+
+
+
+
+
+module.exports = router;
